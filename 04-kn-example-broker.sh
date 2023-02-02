e5e8f0bbc5a05f22fc0b5a51d18ff1540be2c6ac
Matthias Wessendorf <mwessend@redhat.com>
2023-02-02 11:38:47 +0100

    :dizzy: adding logger and a trigger for it
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- 04-kn-example-broker.sh
+++ 04-kn-example-broker.sh
@@ -30,6 +30,55 @@ metadata:
 spec: {}
 EOF
 
+## Setting a debug logger:
+cat <<-EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: log-receiver
+spec:
+  selector:
+    app: log-receiver
+  ports:
+    - port: 80
+      protocol: TCP
+      targetPort: log-receiver
+      name: http
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: log-receiver
+  labels:
+    app: log-receiver
+spec:
+  containers:
+  - name: log-receiver
+    image: quay.io/openshift-knative/knative-eventing-sources-event-display
+    imagePullPolicy: Always
+    ports:
+    - containerPort: 8080
+      protocol: TCP
+      name: log-receiver
+
+EOF
+
+## Setting a debug trigger (all events):
+cat <<-EOF | kubectl apply -f -
+apiVersion: eventing.knative.dev/v1
+kind: Trigger
+metadata:
+  name: log-trigger
+spec:
+  broker: my-demo-kafka-broker
+  subscriber:
+    ref:
+      apiVersion: v1
+      kind: Service
+      name: log-receiver
+EOF
+
 sleep 3
 
 kubectl get brokers.eventing.knative.dev
+kubectl get triggers.eventing.knative.dev
